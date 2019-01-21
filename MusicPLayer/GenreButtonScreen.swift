7e442e8c1280fa941746bc6c7eb470c91d8ae02e
Your Name <you@example.com>
2019-01-21 15:36:51 -0700

    completed project

--- MusicPLayer/GenreButtonScreen.swift
+++ MusicPLayer/GenreButtonScreen.swift
@@ -1,7 +1,10 @@
 
 import UIKit
+import MediaPlayer
 
 class GenreButtonScreen: UIViewController {
+    
+    var musicPLayer = MPMusicPlayerController.applicationMusicPlayer
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -9,10 +12,38 @@ class GenreButtonScreen: UIViewController {
       
     }
     
-    @IBOutlet weak var GenreButtonTapped: UIButton!
+    
+    @IBAction func GenreButtonTapped(_ sender: UIButton) {
+        
+        MPMediaLibrary.requestAuthorization { (status) in
+            if status == .authorized{
+                DispatchQueue.main.async {
+                 self.playGenre(genre: sender.currentTitle!)
+                }
+            }
+        }
+    }
     
     
+    @IBAction func stoButtonTapped(_ sender: UIButton) {
+        musicPLayer.stop()    }
     
+    @IBAction func nextButtonTapped(_ sender: UIButton) {
+        musicPLayer.skipToNextItem()
+    }
+    
+    func playGenre(genre: String){
+        musicPLayer.stop()
+        
+        let query = MPMediaQuery()
+        let predicate = MPMediaPropertyPredicate(value: genre, forProperty: MPMediaItemPropertyGenre)
+        query.addFilterPredicate(predicate)
+        
+        musicPLayer.setQueue(with: query)
+        musicPLayer.shuffleMode = .songs
+        musicPLayer.play()
+        
+    }
 }
     
 
