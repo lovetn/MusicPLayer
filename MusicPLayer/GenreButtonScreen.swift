0590654dc4e00f6b2e5a0ac8e05901ad481f1b93
Lovet Nguatem <loveta1@109c02vx09mj1gc.local>
2019-01-21 14:13:51 -0700

    ere

---
+++ MusicPLayer/GenreButtonScreen.swift
@@ -0,0 +1,19 @@
+
+import UIKit
+
+class GenreButtonScreen: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+      
+    }
+    
+    @IBOutlet weak var GenreButtonTapped: UIButton!
+    
+    
+    
+}
+    
+
+
